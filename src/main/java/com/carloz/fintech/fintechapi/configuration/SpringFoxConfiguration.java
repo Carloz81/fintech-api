16f30767735be577fb8ad8b8d7a144000b0ed05c
Carlo <principe0811@hotmail.it>
2020-03-22 15:08:29 +0100

    Swagger 2 integration
    Swagger 2 integration

---
+++ src/main/java/com/carloz/fintech/fintechapi/configuration/SpringFoxConfiguration.java
@@ -0,0 +1,30 @@
+package com.carloz.fintech.fintechapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger 2 configuration for all the controllers.
+ * This configuration with the selector "any" server
+ * all the API.
+ *
+ * @author Carlo Santovito
+ */
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+    }
+}
